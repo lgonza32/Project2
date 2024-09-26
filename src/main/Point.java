81c02d858a4ed1652d658022d896da319bf40d2c
Laurenz Luke Gonzales <lgonza32@uncc.edu>
2024-09-26 04:03:44 -0400

    created Point class to get x and y coords

---
+++ src/main/Point.java
@@ -0,0 +1,19 @@
+package main;
+
+/**
+ * Represents a point in 2D plane with x and y coordinates.
+ */
+public class Point {
+    int x, y; // Initilize a point as x and y
+
+    /**
+     * Constructor for a point with the x and y coordinates.
+     *
+     * @param x The x coordinate of the point.
+     * @param y The y coordinate of the point.
+     */
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
