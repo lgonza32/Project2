07dcaa9809b25d602d2339a722d001168ff82fa4
Laurenz Luke Gonzales <lgonza32@uncc.edu>
2024-09-26 03:56:55 -0400

    init commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * The main class to implement Convex Hull algorithm using Divide and Conquer
+ * 
+ * @author Laurenz Luke Gonzales
+ *         GWID: G20104564
+ */
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
