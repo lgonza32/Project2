83cf5c70d2100e52881226000585be5b4f7efe58
Laurenz Luke Gonzales <lgonza32@uncc.edu>
2024-09-26 15:23:58 -0400

    created checkCCW method and test cases

--- src/main/ConvexHull.java
+++ src/main/ConvexHull.java
@@ -10,24 +10,25 @@ import java.util.List;
  * The scheme behind Convex Hull, Divide and Conquer for a set P of n points:
  * <ul>
  * <li>Sort n points by x coordinates (n log n).
- * <li>Divide those set of points into two using a median O(1).
+ * <li>Divide those set of points into two subsets by finding a median O(1).
  * <li>Find Convex Hulls of each half recursively O(n log n).
  * <li>Merge the two hulls; O(n)
  * </ul>
  * The Overall Time Complexity should be: O(n log n).
  */
 public class ConvexHull {
-    
+
     /**
-     * Mehod that finds the "east" most point (the point with the largest x value) in the hull .
+     * Mehod that finds the "east" most point (the point with the largest x value)
+     * in the hull.
      * 
      * @param hull A list of points from a Convex Hull.
      * @return The index of the "east" most point in the hull.
      */
     public static int findEastMost(List<Point> hull) {
         int index = 0;
-        for(int i = 1; i < hull.size(); i++) {
-            if(hull.get(i).x > hull.get(index).x) {
+        for (int i = 1; i < hull.size(); i++) {
+            if (hull.get(i).x > hull.get(index).x) {
                 index = i;
             }
         }
@@ -35,15 +36,16 @@ public class ConvexHull {
     }
 
     /**
-     * Method that finds the "west" most point (the point with the smallest x value) in the hull .
+     * Method that finds the "west" most point (the point with the smallest x value)
+     * in the hull .
      * 
      * @param hull A list of points from a Convex Hull.
      * @return The index of the "west" most point in the hull.
      */
     public static int findWestMost(List<Point> hull) {
         int index = 0;
-        for(int i = 1; i < hull.size(); i++) {
-            if(hull.get(i).x < hull.get(index).x) {
+        for (int i = 1; i < hull.size(); i++) {
+            if (hull.get(i).x < hull.get(index).x) {
                 index = i;
             }
         }
@@ -51,15 +53,34 @@ public class ConvexHull {
     }
 
     /**
+     * This method checks to make sure that we are iterating through the cycle in a
+     * counterclockwise direction when finding points on a plane. This helps ensure
+     * that the Convex shape is still a polygon and connections to the points do not
+     * concave.
+     * 
+     * @param a First point
+     * @param b Second point
+     * @param c Third point
+     * @return Positive - if counterclockwise
+     *         Negative - if clockwise
+     *         Zero - if collinear
+     */
+    public static int checkCCW(Point a, Point b, Point c) {
+        return (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
+    }
+
+    /**
      * Method that finds the Convex Hull of a given set of P of n points.
+     * 
      * @param points
      * @return
      */
-    public static List<Point> convexHull(Point[] points) {
+    // public static List<Point> convexHull(Point[] points) {
 
-        // Sort the n points by x coords
-        Arrays.sort(points, Comparator.comparingInt(p -> p.x));
+    // // Sort the n points by x coords to make finding median easier
+    // Arrays.sort(points, Comparator.comparingInt(p -> p.x));
 
-        return points;
-    }
+    // return points;
+    // }
+
 }
--- src/test/ConvexHullTest.java
+++ src/test/ConvexHullTest.java
@@ -8,7 +8,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.List;
 
 /**
- * JUnit tests for the Convex Hull class.
+ * JUnit tests for the ConvexHull class.
+ * Test determines if the methods within the ConvexHull class work as intended
+ * through assertions.
  */
 public class ConvexHullTest {
 
@@ -35,13 +37,13 @@ public class ConvexHullTest {
     }
 
     /**
-     * Test for findWestMost() method
+     * Test for findWestMost() method.
      * Helps determine if method runs correctly by finding the smallest x value in a
      * hull.
      */
     @Test
     public void testFindWestMost() {
-        int westMost;
+        int result;
 
         List<Point> hull = List.of(
                 new Point(0, 0),
@@ -52,8 +54,39 @@ public class ConvexHullTest {
                 new Point(-2, -2),
                 new Point(-3, -3));
 
-        westMost = ConvexHull.findWestMost(hull);
-        assertEquals(6, westMost, "West most point should be at index 6: (-3, -3).");
+        result = ConvexHull.findWestMost(hull);
+        assertEquals(6, result, "West most point should be at index 6: (-3, -3).");
     }
 
+    /**
+     * Test for checkCCW() method.
+     * Helps determine if we are able to iterate through a set of hulls
+     */
+    @Test
+    public void testCheckCCW() {
+        int resultCCW, resultCW, resultCollinear;
+
+        // Test CCW
+        Point a = new Point(0, 0);
+        Point b = new Point(1, 1);
+        Point c = new Point(0, 2);
+        resultCCW = ConvexHull.checkCCW(a, b, c);
+
+        // Test CW
+        Point d = new Point(0, 0);
+        Point e = new Point(1, 1);
+        Point f = new Point(2, 0);
+        resultCW = ConvexHull.checkCCW(d, e, f);
+
+        // Test Collinear
+        Point g = new Point(0, 0);
+        Point h = new Point(1, 1);
+        Point i = new Point(2, 2);
+        resultCollinear = ConvexHull.checkCCW(g, h, i);
+
+        assertAll("Testing test cases for checkCCW()",
+                () -> assertTrue(resultCCW > 0, "Expected counterclockwise orientation."),
+                () -> assertTrue(resultCW < 0, "Expected clockwise orientation."),
+                () -> assertEquals(0, resultCollinear, "Expected collinear points."));
+    }
 }
