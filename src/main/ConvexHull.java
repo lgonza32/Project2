a72c771e38036888a1e30e8238daced655d7299f
Laurenz Luke Gonzales <lgonza32@uncc.edu>
2024-09-27 12:49:08 -0400

    change javadocs

--- src/main/ConvexHull.java
+++ src/main/ConvexHull.java
@@ -10,7 +10,6 @@ import java.util.List;
  * This class provides methods to find and merge Convex Hulls on a 2D plane.
  * The scheme behind Convex Hull, Divide and Conquer for a set P of n points:
  * <ul>
- * <li>Sort n points by x coordinates (n log n).
  * <li>Divide those set of points into two subsets by finding a median O(1).
  * <li>Find Convex Hulls of each half recursively O(n log n).
  * <li>Merge the two hulls; O(n)
